0bd15ee42a4cb406857b63c247b4f76761b3c0d2
Maxim-Bernashevsky <0112358bern@mail.ru>
2017-05-31 15:28:25 +0300

    source map off

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var path = require('path');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
+    //devtool: 'cheap-module-eval-source-map',
     entry: [
         //'webpack-hot-middleware/client',
         'babel-polyfill',
